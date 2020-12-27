db9eb713a312f485bb246164c45cf70f95f45d06
Oscar Osses <oscarossesa@gmail.com>
2020-12-27 13:47:45 -0300

    CalendarApp v1

---
+++ public/precache-manifest.11cabeb14be0e5f458e6bb8d68811697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a66765c1998636bce1248851bd8658f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "292fe1d09dde732bd6f5",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "a1d595ee3b017528b250",
+    "url": "/static/css/main.b68bc1de.chunk.css"
+  },
+  {
+    "revision": "292fe1d09dde732bd6f5",
+    "url": "/static/js/2.fdfaeb60.chunk.js"
+  },
+  {
+    "revision": "62dc9784057de1bc85a8684fec455f1b",
+    "url": "/static/js/2.fdfaeb60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d595ee3b017528b250",
+    "url": "/static/js/main.c220bcc7.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
